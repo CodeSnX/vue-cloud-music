2fa35287c2e69d416a36e84a3f1a03f5fc99f389
CodeJasmine <2816540069@qq.com>
2020-04-20 22:47:36 +0800

    第一次推送

---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <h1>404</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: '404',
+  data() {
+    return {}
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8023,
+    https: false,
+    open: true,
+    hotOnly: true
+  }
+}
